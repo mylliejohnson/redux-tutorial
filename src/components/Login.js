5cfac0af3a479ee331386b41062cafcdeafcd1f3
mylliejohnson <mylliejohnson@gmail.com>
2022-05-10 12:59:18 -0400

    following redux tutorial, created user and theme reducers

---
+++ src/components/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useDispatch } from 'react-redux'
+import { login, logout } from '../features/user'
+
+
+function Login(props) {
+
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+            <button onClick={() => {
+                dispatch(
+                    login({ name: "Myllie", age: 25, email: "myllie@gmail.com" })
+                )
+            }}>Login</button>
+
+            <button onClick={() => { dispatch(logout())}}>
+                Logout
+            </button>
+        </div>
+    );
+}
+
+export default Login;
